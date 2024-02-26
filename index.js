e47bd361d41d5e3e87dbfd3034145856569e8eee
Gugun Gunawan <gugun.net21@gmail.com>
2024-02-26 16:24:38 +0700

    add response

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.use(()=>{
+app.use((req, res) => {
     console.log('heav req');
+    res.send({msg:'hello world'});
 });
 app.listen(8080, () => console.log("Server is running  on port http://localhost:8080"));
